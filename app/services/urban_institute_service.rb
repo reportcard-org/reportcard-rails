3214a9dcf01eaf30902ad0c6cb29ee7bedc5c6d0
Alex Boyd <alexanderleeboyd@gmail.com>
2022-10-16 16:36:19 -0400

    Test: get_district_financial_data returns a response

--- app/services/urban_institute_service.rb
+++ app/services/urban_institute_service.rb
@@ -6,4 +6,10 @@ class UrbanInstituteService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.get_district_financial_data(lea_id)
+    response = BaseService.urban_institute_connection.get("api/v1/school-districts/ccd/finance/2018/?leaid=#{lea_id}")
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
 end
--- spec/service/urban_institute_service_spec.rb
+++ spec/service/urban_institute_service_spec.rb
@@ -24,4 +24,23 @@ RSpec.describe UrbanInstituteService do
     end
   end
 
+  describe "#get_district_financial_data" do
+    it 'returns a response' do
+      response = UrbanInstituteService.get_district_financial_data(1100030)
+
+      expect(response).to be_a(Hash)
+
+      expect(response).to have_key(:results)
+      expect(response[:results]).to be_an(Array)
+
+      expect(response[:results].first).to be_a(Hash)
+
+      expect(response[:results].first).to have_key(:salaries_instruction)
+      expect(response[:results].first[:salaries_instruction]).to be_an(Integer)
+
+      expect(response[:results].first).to have_key(:salaries_total)
+      expect(response[:results].first[:salaries_total]).to be_an(Integer)
+    end
+  end
+
 end
