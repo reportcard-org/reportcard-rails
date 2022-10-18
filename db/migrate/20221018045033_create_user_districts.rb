24121f1eeb0b3db106c0a31646d88f48bfbab1f1
Drew MacNicholas <dmacnicholas@gmail.com>
2022-10-17 23:52:12 -0500

    Update migration for UserDistricts Table

--- db/migrate/20221018045033_create_user_districts.rb
+++ db/migrate/20221018045033_create_user_districts.rb
@@ -1,8 +1,8 @@
 class CreateUserDistricts < ActiveRecord::Migration[5.2]
   def change
     create_table :user_districts do |t|
-      t.references :users, foreign_key: true
-      t.references :districts, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :district, foreign_key: true
     end
   end
 end
