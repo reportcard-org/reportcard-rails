6522931666fe502c6929d53775aba0077800eb96
Alex Boyd <alexanderleeboyd@gmail.com>
2022-10-20 14:14:35 -0400

    Feat: add additional data as fields on district model

--- db/migrate/20221018035723_create_districts.rb
+++ db/migrate/20221018035723_create_districts.rb
@@ -3,6 +3,28 @@ class CreateDistricts < ActiveRecord::Migration[5.2]
     create_table :districts do |t|
       t.string :name
       t.integer :lea_id
+      t.string :urban_centric_locale
+      t.integer :number_of_schools_in_district
+      t.integer :enrollment
+      t.integer :pre_k_teachers
+      t.integer :kindergarten_teachers
+      t.integer :elementary_teachers
+      t.integer :secondary_teachers
+      t.integer :total_teachers
+      t.float :student_teacher_ratio
+      t.integer :instructional_aides
+      t.integer :guidance_counselors
+      t.float :student_guidance_counselor_ratio
+      t.integer :librarian_total
+      t.float :student_librarian_ratio
+      t.integer :total_staff
+      t.integer :district_expense_total
+      t.integer :expenses_for_instruction
+      t.integer :salaries_total
+      t.integer :salaries_instruction
+      t.float :instruction_salary_percent_of_total
+      t.float :per_student_expenditure
+      t.float :per_teacher_salary_expenses
     
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,28 @@ ActiveRecord::Schema.define(version: 2022_10_18_045033) do
   create_table "districts", force: :cascade do |t|
     t.string "name"
     t.integer "lea_id"
+    t.string "urban_centric_locale"
+    t.integer "number_of_schools_in_district"
+    t.integer "enrollment"
+    t.integer "pre_k_teachers"
+    t.integer "kindergarten_teachers"
+    t.integer "elementary_teachers"
+    t.integer "secondary_teachers"
+    t.integer "total_teachers"
+    t.float "student_teacher_ratio"
+    t.integer "instructional_aides"
+    t.integer "guidance_counselors"
+    t.float "student_guidance_counselor_ratio"
+    t.integer "librarian_total"
+    t.float "student_librarian_ratio"
+    t.integer "total_staff"
+    t.integer "district_expense_total"
+    t.integer "expenses_for_instruction"
+    t.integer "salaries_total"
+    t.integer "salaries_instruction"
+    t.float "instruction_salary_percent_of_total"
+    t.float "per_student_expenditure"
+    t.float "per_teacher_salary_expenses"
   end
 
   create_table "user_districts", force: :cascade do |t|
