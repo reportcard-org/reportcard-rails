91a20e43cf81f2950c56fab79e55fa4b1050d104
Drew MacNicholas <dmacnicholas@gmail.com>
2022-10-26 16:39:46 -0500

    Add in validation for number of school districts
    Co-authored-by: <benjsilverstein@comcast.net>

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe District, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :lea_id}
     it { should allow_value(nil).for(:urban_centric_locale) }
+    it { should allow_value(nil).for(:number_of_schools_in_district) }
 
   end
 
